efe4d1ff4bd4f65a54ce9f2016fd9cb5820999e6
Leonid-Developer <149823561+Leonid-Developer@users.noreply.github.com>
2023-11-03 21:21:10 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class HelloWorld : MonoBehaviour
+{
+    //Текст к которому прибавляется int money
+    public Text text;
+
+    //Переменная money
+    public int money;
+
+    private void Update()
+    {
+        text.text = "Money : " + money.ToString();
+    }
+
+    //Функция по добавлению монет
+    public void OnMoney()
+    {
+        money += 1;
+    }
+}
